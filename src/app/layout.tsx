948569961aadcffc458432b1e5163cea96dbe7f8
redarasmy <red7mer@gmail.com>
2025-06-11 17:47:56 +0100

    setup next-auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import StoreProvider from "@/components/providers/store-provider";
+import { getServerSession } from "next-auth";
+import SessionProvider from '@/components/providers/session-provider'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,18 +20,21 @@ export const metadata: Metadata = {
   description: "",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await getServerSession()
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <StoreProvider>
-          {children}
+          <SessionProvider session={session} >
+            {children}
+          </SessionProvider>
         </StoreProvider>
       </body>
     </html>
