6fbe84c85b1e4241ec19796e18e5ae8797be6b43
redarasmy <red7mer@gmail.com>
2025-06-16 22:51:52 +0100

    add link to the home page in nav-menu

--- src/components/nav-menu.tsx
+++ src/components/nav-menu.tsx
@@ -4,16 +4,17 @@ import { signIn , signOut , useSession } from "next-auth/react";
 import { Button } from "./ui/button";
 import { LocaleSelect } from "./locale-select";
 import Image from "next/image";
+import { Link } from "@/i18n/navigation";
 
 export default function NavMenu() {
     const {data : session} = useSession()
     return (
         <div className="flex items-center gap-4 md:py-3 py-2 md:px-4 px-2 bg-accent justify-between">
 
-            <div className="flex items-center gap-2 ">
+            <Link className="flex items-center gap-2 " href={'/'}>
                 <Image alt="chess-piece" src="/images/red-rook.png" width={40} height={40}/>
                 <h1 className="font-bold md:text-2xl text-xl font-serif tracking-tighter ">Bloody Chess</h1>
-            </div>
+            </Link>
 
             <div className="flex gap-2 md:gap-4">
                 <LocaleSelect/>
