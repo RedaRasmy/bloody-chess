b0ec50ff54892a89c38b4f82421d9adf80bbcfdc
redarasmy <red7mer@gmail.com>
2025-06-29 02:59:19 +0100

    add replay button in game-details and let engineaction infinitly recursive

--- src/features/gameplay/server-actions/chess-engine.ts
+++ src/features/gameplay/server-actions/chess-engine.ts
@@ -4,11 +4,16 @@ import { EngineResponse } from "../types"
 
 
 export async function getEngineResponse(fen:string,level:number):Promise<EngineResponse> {
-
-    const res = await fetch(`https://stockfish.online/api/s/v2.php?fen=${encodeURIComponent(fen)}&depth=${level}`)
-    if (!res.ok) {
-        throw new Error("Network response was not ok")
+    try {
+        const res = await fetch(`https://stockfish.online/api/s/v2.php?fen=${encodeURIComponent(fen)}&depth=${level}`)
+        if (!res.ok) {
+            throw new Error("Network response was not ok")
+        }
+        return await res.json()
+    } catch (error) {
+        console.log(error)
+        return getEngineResponse(fen,level)
     }
-    return await res.json()
+
 }
 
