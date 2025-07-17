bcb861c54c2fad1cddf01fdfa1711d909b2816e1
redarasmy <red7mer@gmail.com>
2025-07-17 21:35:05 +0100

    refactor signin page

--- src/app/[locale]/auth/register/page.tsx
+++ src/app/[locale]/auth/register/page.tsx
@@ -1,11 +1,7 @@
 
-// import { signIn } from 'next-auth/react';
-// import { useState } from 'react';
-// import { useRouter } from 'next/navigation';
-
 import { getServerSession } from "next-auth";
-import RegisterForm from './register-form'
 import {redirect} from 'next/navigation'
+import RegisterForm from './register-form'
 
 export default async function RegisterPage() {
   const session = await getServerSession()
