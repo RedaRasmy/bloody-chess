15d6fe4a1949be9402e5b239f8ad88c6c2c8fa5c
redarasmy <red7mer@gmail.com>
2025-07-17 20:22:45 +0100

    enhance signin form

--- src/app/[locale]/auth/signin/page.tsx
+++ src/app/[locale]/auth/signin/page.tsx
@@ -1,54 +1,135 @@
-'use client';
+"use client"
+import { Button } from "@/components/ui/button"
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { signIn, useSession } from "next-auth/react"
+import { useRouter } from "next/navigation"
+import { Link } from "@/i18n/navigation"
+import { useEffect } from "react"
 
-import { signIn } from 'next-auth/react';
-import { useState } from 'react';
-import { useRouter } from 'next/navigation';
+const formSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(8, "Password must be at least 8 characters."),
+})
 
 export default function SignInPage() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState<string | null>(null);
-  const router = useRouter();
+    const session = useSession()
+    const router = useRouter()
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    try {
-      const result = await signIn('credentials', {
-        email,
-        password,
-        mode: 'signin',
-        redirect: false,
-      });
+    useEffect(() => {
+        if (session.status === "authenticated") {
+            router.push("/")
+        }
+    }, [session.status, router])
 
-      if (result?.error) {
-        setError(result.error);
-      } else {
-        router.push('/');
-      }
-    } catch (err) {
-      setError('Unexpected error occurred 🤕');
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+        },
+    })
+
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        const { email, password } = values
+        try {
+            const result = await signIn("credentials", {
+                email,
+                password,
+                mode: "signin",
+                redirect: false,
+            })
+
+            if (result?.error) {
+                form.setError("root", { message: result.error })
+            } else {
+                router.push("/")
+            }
+        } catch (err) {
+            form.setError("root", { message: "Unexpected error occurred 🤕" })
+        }
     }
-  };
 
-  return (
-    <form onSubmit={handleSubmit} className='gap-3 h-full flex flex-col items-center justify-center'>
-      <h2 className='text-3xl text-start mb-2'>Sign In</h2>
-      <input
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        required
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        required
-      />
-      {error && <p style={{ color: 'red' }}>{error}</p>}
-      <button type="submit">Sign In</button>
-    </form>
-  );
+    const errors = form.formState.errors
+    const message =
+        errors.root?.message ??
+        errors.email?.message ??
+        errors.password?.message ??
+        null
+
+    return (
+        <Form {...form}>
+            <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-5 my-auto place-self-center w-[min(90%,400px)]"
+            >
+                <div>
+                    <h1 className="text-3xl">Sign In</h1>
+                    <p className="text-red-500 my-2">
+                        {message}
+                    </p>
+                </div>
+                <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Email</FormLabel>
+                            <FormControl>
+                                <Input
+                                    placeholder="Enter your email"
+                                    type="email"
+                                    {...field}
+                                />
+                            </FormControl>
+                            <FormDescription></FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Password</FormLabel>
+                            <FormControl>
+                                <Input
+                                    placeholder="Enter your password"
+                                    type='password'
+                                    {...field}
+                                />
+                            </FormControl>
+                            <FormDescription>
+                                <Link href={"/auth/reset"}>
+                                    Forget password?
+                                </Link>
+                            </FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <div className="flex flex-row-reverse mt-6">
+                    <Button
+                        type="submit"
+                        className=""
+                        disabled={form.formState.isSubmitting}
+                    >
+                        Submit
+                    </Button>
+                </div>
+            </form>
+        </Form>
+    )
 }
