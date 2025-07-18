eeab33099ad10c71a8175b3c45f0b4df0076c446
redarasmy <red7mer@gmail.com>
2025-07-18 17:18:32 +0100

    implement usePlayer hook

--- src/features/gameplay/hooks/use-player.tsx
+++ src/features/gameplay/hooks/use-player.tsx
@@ -1,63 +1,68 @@
 import { useSession } from "next-auth/react"
 import { useState, useEffect } from "react"
-import {getPlayer} from '../server-actions/player-actions'
-import {Player,Guest} from '@/db/types'
+import { getPlayer } from "../server-actions/player-actions"
+import { getGuest,createGuest } from "../server-actions/guest-actions"
+import { Player, Guest } from "@/db/types"
+import { useLocalStorage } from 'usehooks-ts'
 
-export default function usePlayer(){
-    const {status,data} = useSession()
-    const [player, setPlayer] = useState<Player|null>(null)
-    const [guest, setGuest] = useState<Guest|null>(null)
+export default function usePlayer() {
+    const { status, data } = useSession()
+    const [player, setPlayer] = useState<Player | null>(null)
+    const [guest, setGuest] = useState<Guest | null>(null)
     const [isLoading, setIsLoading] = useState(true)
 
     useEffect(() => {
-        if (status === 'loading') return;
+        if (status === "loading") return;
 
         async function getPlayerOrGuest() {
-            if (status === 'authenticated') {
-                const playerId = data.user.playerId
-                const player = await getPlayer(playerId)
+            try {
+                if (status === "authenticated") {
+                    const playerId = data.user.playerId // or playerId if you added it
 
-                if (!player) throw new Error("player not exists!");
+                    const playerData = await getPlayer(playerId)
+                    if (!playerData) throw new Error("Player not exists in DB! id="+playerId)
 
-                setPlayer(player)
-                return {
-                    isLoading : false,
-                    type : 'player',
-                    player
+                    setPlayer(playerData)
+                } else {
+                    const [id, setId] = useLocalStorage('guest_id','none')
+                    if (id!=='none') { // guest exists
+                        const guest = await getGuest(id)
+                        if (!guest) throw new Error("Guest not exists in DB! id="+id);
+                        setGuest(guest)
+                    } else {
+                        // create new one
+                        const newGuest = await createGuest()
+                        setId(newGuest.id)
+                        setGuest(newGuest)
+                    }
                 }
-            } else {
-                // check local storage
-                // if exist id fetch existed guest
-                // else create new one
-
-
-
-
+            } catch (error) {
+                console.error(error)
+            } finally {
+                setIsLoading(false)
             }
-            setIsLoading(false)
         }
+
         getPlayerOrGuest()
- 
-    }, [status])
+    }, [status, data])
 
     if (isLoading) {
         return {
-            isLoading : true,
-        }
-    } else {
-        if (player) {
-            return {
-                isLoading : false,
-                type : 'player',
-                player 
-            }
-        } else {
-            return {
-                isLoading: false,
-                type : 'guest',
-                guest
-            }
+            isLoading: true,
         }
     }
 
+    if (player) {
+        return {
+            isLoading: false,
+            type: "player",
+            player,
+        }
+    }
+
+    return {
+        isLoading: false,
+        type: "guest",
+        guest,
+    }
 }
